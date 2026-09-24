d36ec0d84633a3b0cc0ae3c1aaa4b6f9df3d2530
DanielKei-Salle <dk.marquez@lasallistas.org.mx>
2026-09-24 14:31:21 -0600

    Add files via upload
    La segunda version de el proyecto en C,   ya termine de añadir los grafos conectados y empeze a hacer las funciones basicas

--- Grafos de Almacen/main.c
+++ Grafos de Almacen/main.c
@@ -10,48 +10,226 @@
     Esta simulacion se hara mediante el uso de grafos como representacion de las salas y pasillos conectados
 */
 
-struct roomPos
-{
-    struct roomPos *nextRoom;
-    struct roomPos *joinRoom;
-    char nameInfo[30];
-};
 
+//-------------------Apuntadores para los pasillos entre las distintas salas
 struct conctWay
 {
+    struct conctWay *nextWay;
+    struct roomPos *conctRoom;
+    int weight;
+};
+
+//---------------------Estructura para los apuntadores de las salas
+struct roomPos
+{
+    struct roomPos *nextRoom, *backRoom;
+    struct conctWay *startWay;
+    char nameInfo[30];
+    short int blocked;
 
 };
 
 
-int main()
+//-----------------------------------------LLenar de manera rapida el mapa y conexiones
+void fillMap(struct roomPos **startRoom)
 {
-    struct roomPos *startRoom, *moveRoom;
-    int cl1;
-    startRoom = (struct roomPos*) malloc(sizeof (struct roomPos));
-    moveRoom = startRoom;
+    short int cl1,cl2;
+    struct roomPos *moveRoom;
 
-    for(cl1=0;cl1<4;cl1++)
+    *startRoom = (struct roomPos*) malloc(sizeof (struct roomPos));         //Crear el primer espacio para el primer apuntador
+    moveRoom = *startRoom;
+    for(cl1=0;cl1<8;cl1++)          //Ciclo que repite 8 veces para llenar todos los datos
     {
-        switch(cl1)
+        moveRoom->blocked = 0;
+        switch(cl1)         //Escoje los datos a llenar de cada uno de los apuntadoes
         {
-        case 0:     strcpy(moveRoom->nameInfo, "xddd");     break;
+        case 0:     strcpy(moveRoom->nameInfo, "Estanterias");     break;
         case 1:     strcpy(moveRoom->nameInfo, "Zona de carga");    break;
         case 2:     strcpy(moveRoom->nameInfo, "Control de calidad");   break;
-        case 3:     strcpy(moveRoom->nameInfo, "Puertas de salida");    break;
+        case 3:     strcpy(moveRoom->nameInfo, "Zona de descarga");    break;
+        case 4:     strcpy(moveRoom->nameInfo, "Area de empaquetado");    break;
+        case 5:     strcpy(moveRoom->nameInfo, "Almacen frio");    break;
+        case 6:     strcpy(moveRoom->nameInfo, "Mantenimiento");    break;
+        case 7:     strcpy(moveRoom->nameInfo, "Puertas de salida");    break;
         }
-        if(cl1!=3)
+        if(cl1!=7)          //Crea un nuevo espacio vacio al siguiente apuntador si aun queda espacio del ciclo
         {
             moveRoom->nextRoom = (struct roomPos*) malloc(sizeof (struct roomPos));
+            moveRoom->nextRoom->backRoom = moveRoom;
             moveRoom = moveRoom->nextRoom;
         }else   moveRoom->nextRoom = NULL;
     }
 
-    moveRoom = startRoom;
-    while(moveRoom!=NULL)
+    struct conctWay *moveWay;
+    moveRoom = *startRoom;
+    for(cl1=0;cl1<8;cl1++)
     {
-        printf("[%s]",moveRoom->nameInfo);
+        moveRoom->startWay = (struct conctWay*) malloc(sizeof (struct conctWay));
+        moveWay = moveRoom->startWay;
+        switch(cl1)
+        {
+            //----Coinfiguracion de Estanteria
+        case 0:
+            moveWay->weight = 3;
+            moveWay->conctRoom = moveRoom->nextRoom;
+            moveWay->nextWay = (struct conctWay*) malloc(sizeof (struct conctWay));
+            moveWay = moveWay->nextWay;
+            moveWay->weight = 1;
+            moveWay->conctRoom = moveRoom->nextRoom->nextRoom;
+            moveWay->nextWay = NULL;
+            break;
+
+            //----Coinfiguracion de Zona de carga
+        case 1:
+            moveWay->weight = 4;
+            moveWay->conctRoom = moveRoom->nextRoom;
+            moveWay->nextWay = (struct conctWay*) malloc(sizeof (struct conctWay));
+            moveWay = moveWay->nextWay;
+            moveWay->weight = 5;
+            moveWay->conctRoom = moveRoom->nextRoom->nextRoom;
+            moveWay->nextWay = (struct conctWay*) malloc(sizeof (struct conctWay));
+            moveWay = moveWay->nextWay;
+            moveWay->weight = 2;
+            moveWay->conctRoom = moveRoom->nextRoom->nextRoom->nextRoom;
+            moveWay->nextWay = NULL;
+            break;
+
+            //----Coinfiguracion de cONTROL de calidad
+        case 2:
+            moveWay->weight = 25;
+            moveWay->conctRoom = moveRoom->nextRoom;
+            moveWay->nextWay = (struct conctWay*) malloc(sizeof (struct conctWay));
+            moveWay = moveWay->nextWay;
+            moveWay->weight = 17;
+            moveWay->conctRoom = moveRoom->nextRoom->nextRoom->nextRoom;
+            moveWay->nextWay = NULL;
+            break;
+
+            //----Configuracion de Zona de descarga
+        case 3:
+            moveWay->weight = 5;
+            moveWay->conctRoom = moveRoom->backRoom->backRoom;
+            moveWay->nextWay = (struct conctWay*) malloc(sizeof (struct conctWay));
+            moveWay = moveWay->nextWay;
+            moveWay->weight = 7;
+            moveWay->conctRoom = moveRoom->nextRoom->nextRoom->nextRoom;
+            moveWay->nextWay = NULL;
+            break;
+
+            //----Configuracion de Area de Empaquetado
+        case 4:
+            moveWay->weight = 6;
+            moveWay->conctRoom = moveRoom->nextRoom;
+            moveWay->nextWay = (struct conctWay*) malloc(sizeof (struct conctWay));
+            moveWay = moveWay->nextWay;
+            moveWay->weight = 17;
+            moveWay->conctRoom = moveRoom->nextRoom->nextRoom->nextRoom;
+            moveWay->nextWay = NULL;
+            break;
+
+        case 5:
+            moveWay->weight = 7;
+            moveWay->conctRoom = moveRoom->backRoom;
+            moveWay->nextWay = (struct conctWay*) malloc(sizeof (struct conctWay));
+            moveWay = moveWay->nextWay;
+            moveWay->weight = 15;
+            moveWay->conctRoom = moveRoom->nextRoom;
+            moveWay->nextWay = (struct conctWay*) malloc(sizeof (struct conctWay));
+            moveWay = moveWay->nextWay;
+            moveWay->weight = 8;
+            moveWay->conctRoom = moveRoom->nextRoom->nextRoom;
+            moveWay->nextWay = NULL;
+            break;
+
+        case 6:
+            moveWay->weight = 8;
+            moveWay->conctRoom = moveRoom->backRoom->backRoom->backRoom;
+            moveWay->nextWay = NULL;
+            break;
+
+        case 7:
+            moveRoom->startWay = NULL;
+            break;
+        }
         moveRoom = moveRoom->nextRoom;
     }
+
+
+    moveRoom = *startRoom;
+    while(moveRoom!=NULL)           //Los imprime para comprobar que si funciona
+    {
+        moveWay = moveRoom->startWay;
+        printf("[%s][%hd]   ",moveRoom->nameInfo, moveRoom->blocked);
+        while(moveWay!=NULL)
+        {
+            printf("(%s)(%d)  ",moveWay->conctRoom->nameInfo, moveWay->weight);
+            moveWay = moveWay->nextWay;
+            //scanf("%hd",&cl1);
+        }
+        printf("\n");
+        moveRoom = moveRoom->nextRoom;
+    }
+
+}
+
+
+//----------------------Determinar sala con menor numero de conexiones
+void checkWays(struct roomPos *startRoom)
+{
+    struct roomPos *moveRoom = startRoom, *extRoom = startRoom;
+    struct conctWay *moveWay;
+    int maxWay = 0,countWay;
+
+    while(moveRoom!=NULL)
+    {
+        countWay = 0;
+        moveWay = moveRoom->startWay;
+        while(moveWay!=NULL)
+        {
+            countWay++;
+            moveWay = moveWay->nextWay;
+        }
+        if(countWay>=maxWay)
+        {
+            extRoom = moveRoom;
+            maxWay = countWay;
+        }
+        moveRoom = moveRoom->nextRoom;
+    }
+    printf("Sala con mas conexiones [%s]",extRoom->nameInfo);
+}
+
+
+void blockRoom(struct roomPos **startRoom)
+{
+    struct roomPos *moveRoom = *startRoom;
+}
+
+
+
+//----------------El mismisimo main
+int main()
+{
+    struct roomPos *startRoom, *moveRoom, *extRoom;
+    struct conctWay *moveWay;
+    fillMap(&startRoom);
+
+    short int menu;
+
+
+    do
+    {
+        printf("\n\n)Realizar simulacion de viaje\n)Bloquear ubicacion\n)Identificar nodo de mayor conexiones\n)");
+        scanf("%hd",&menu);
+        switch(menu)
+        {
+        case 1:
+            checkWays(startRoom);
+        case 2:
+            blockRoom(&startRoom);
+            break;
+        }
+    }while(menu!=-1);
 
     return 0;
 }
